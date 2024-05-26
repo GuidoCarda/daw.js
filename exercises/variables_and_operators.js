b0ad8fa6ecb2876ad5b0e7f14dad8417c13b7fd0
Guido Cardarelli <guidocarda@hotmail.com>
2024-05-25 23:57:11 -0300

    resolve variables_and_operators exercises

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,27 @@
+// 1. Variables y Operadores
+
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor
+// de la suma de ambos números en una 3er variable.
+
+var num1 = 5;
+var num2 = 5;
+
+var sum = num1 + num2;
+console.log(sum);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una
+// 3er variable.
+
+var nombre = "Guido";
+var apellido = "Cardarelli";
+var nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de
+// letras del string) guardando el resultado de la suma en una 3er variable (utilizar
+// length).
+
+var color = "Verde";
+var opacidad = "Oscuro";
+var numLetras = color.length + opacidad.length;
+console.log(numLetras);
